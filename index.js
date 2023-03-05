c954e5dbd096173fc0e2d854ad0025d20c77c28e
John Oziegbe <82390694+oseji@users.noreply.github.com>
2023-03-05 13:26:45 +0100

    added functionality for displaying results

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 "use strict";
 
-const link = `https://api.dictionaryapi.dev/api/v2/entries/en/dog
-`;
+let apiSearchWord;
+// const link = `https://api.dictionaryapi.dev/api/v2/entries/en/${apiSearchWord}
+// `;
 
-const request = new XMLHttpRequest();
+const searchInput = document.querySelector(".word-input");
+const searchBtn = document.querySelector(".search");
+const resultHeading = document.querySelector(".results-heading");
+const searchedWordDisplay = document.querySelector(".searched-word");
+const searchResult = document.querySelector(".search-result");
+const wordResult = document.querySelector(".searched-text");
 
-request.open("get", link);
-request.send();
+// console.log(
+//   searchInput,
+//   searchBtn,
+//   resultHeading,
+//   searchedWordDisplay,
+//   searchResult
+// );
 
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-  const result1 = data.meanings[1].definitions[0].definition;
+//CODE
 
-  console.log(result1);
-  console.log(data.meanings);
+searchBtn.addEventListener("click", function () {
+  apiSearchWord = searchInput.value;
+  console.log(apiSearchWord);
 
-  data.meanings.forEach(function (i) {
-    const partsOfSpeech = i.partOfSpeech;
-    const definitions = i.definitions;
-    console.log(partsOfSpeech);
-    console.log(definitions);
+  const request = new XMLHttpRequest();
+  request.open(
+    "get",
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${apiSearchWord}
+   `
+  );
+  request.send();
 
-    definitions.forEach(function (i) {
-      console.log(i.definition);
-    });
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    //console.log(`data`, data);
+
+    //console.log(data.meanings);
+    const partsOfSpeech = data.meanings[0].partOfSpeech;
+    const textDefinition = data.meanings[0].definitions[0].definition;
+    const phonetics =
+      data.phonetic === undefined ? data.phonetics[1].text : data.phonetic;
+    //console.log("phonetic", phonetics);
+
+    //changing info on the screen
+    resultHeading.innerHTML = `Results for ${apiSearchWord.toUpperCase()}`;
+    //
+    searchedWordDisplay.innerHTML = `${apiSearchWord.toUpperCase()} | ${partsOfSpeech.toUpperCase()} | ${phonetics}`;
+    //
+    wordResult.innerHTML = `${textDefinition}`;
   });
 });
